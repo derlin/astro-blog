c80eda608d0e8a4887dbcefbf410db537d28351e
Lucy Linder <lucy.derlin@gmail.com>
2026-04-05 14:50:24 +0200

    fix: ensure canonical always points to blog.derlin.ch + block robots on github

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,12 @@ export const SITE_TITLE = "DERLIN.";
 export const SITE_DESCRIPTION =
   "A blog about code, dev tools, and occasionally other things.";
 
+// Distinguish between development and production environments
+export const CANONICAL_BASE_URL = "https://blog.derlin.ch/";
 
 // This ensures you get a clean 'https://domain.com/base/' without double slashes
-export const FULL_BASE_URL = new URL(import.meta.env.BASE_URL, import.meta.env.SITE).href;
+export const FULL_BASE_URL = new URL(
+  import.meta.env.BASE_URL,
+  import.meta.env.SITE,
+).href;
+export const IS_LIVE = FULL_BASE_URL.includes(CANONICAL_BASE_URL);
