6d227abd856eb6f3ea95a11a80e937f3e8d33606
Lucy Linder <lucy.derlin@gmail.com>
2026-04-05 11:00:15 +0200

    chore: add biome formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,47 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
-import rehypeExternalLinks from 'rehype-external-links';
-import rehypeCallouts from 'rehype-callouts';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeCallouts from "rehype-callouts";
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || 'http://localhost:4321',
-	integrations: [mdx(), sitemap()],
-	trailingSlash: "never",
-	markdown: {
-		rehypePlugins: [
-			rehypeSlug,
-			rehypeCallouts,
-			[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
-			[rehypeAutolinkHeadings, {
-				behavior: 'prepend',
-				properties: { class: 'anchor', ariaHidden: 'true', tabIndex: -1 },
-				content: { type: 'text', value: '#' },
-			}],
-		],
-		shikiConfig: {
-			theme: 'github-dark',
-		},
-	},
-	build: {
-		format: 'file' // This outputs 'about.html' instead of 'about/index.html'
-	},
-	vite: {
-		build: {
-			rollupOptions: {
-				external: ['/pagefind/pagefind.js'],
-			},
-		},
-	},
+  site: process.env.SITE_URL || "http://localhost:4321",
+  integrations: [mdx(), sitemap()],
+  trailingSlash: "never",
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeCallouts,
+      [
+        rehypeExternalLinks,
+        { target: "_blank", rel: ["noopener", "noreferrer"] },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          properties: { class: "anchor", ariaHidden: "true", tabIndex: -1 },
+          content: { type: "text", value: "#" },
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "github-dark",
+    },
+  },
+  build: {
+    format: "file", // This outputs 'about.html' instead of 'about/index.html'
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["/pagefind/pagefind.js"],
+      },
+    },
+  },
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'DERLIN.';
-export const SITE_DESCRIPTION = 'A blog about code, dev tools, and occasionally other things.';
+export const SITE_TITLE = "DERLIN.";
+export const SITE_DESCRIPTION =
+  "A blog about code, dev tools, and occasionally other things.";
